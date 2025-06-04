4bab03e19f87236a908c34b4a972721431db7426
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2025-06-04 23:24:04 +0530

    done

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ let applybtn = document.querySelector('.apply-btn');
 let colorInput = document.querySelector('#colorInput');
 let color = document.querySelector('.color');
 let container = document.querySelector('.container');
+const colorarray = ['red', 'blue', 'green', 'yellow', 'purple', 'orange','pink', 'brown', 'cyan', 'magenta'];
+const randomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colorarray.length);
+    return colorarray[randomIndex];
+}
+
 const colorChanger = (givencolor) => {
     container.style.backgroundColor = givencolor;
     color.innerText = givencolor;
 }
 const rondombtnclick = () => {
-    console.log("Random button clicked");
+    const randomColorValue = randomColor();
+    colorChanger(randomColorValue);
 }
 const applybtnclick = () => {
     const givencolor = colorInput.value;
