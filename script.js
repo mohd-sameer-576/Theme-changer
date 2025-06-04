548ddfe0642ca50d6a43d2c724ce2c152e41b745
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2025-06-04 23:04:09 +0530

    apply button done

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ let applybtn = document.querySelector('.apply-btn');
 let colorInput = document.querySelector('#colorInput');
 let color = document.querySelector('.color');
 let container = document.querySelector('.container');
+const colorChanger = (color) => {
+    container.style.backgroundColor = color;
+}
 const rondombtnclick = () => {
     console.log("Random button clicked");
 }
 const applybtnclick = () => {
-    console.log("apply button clicked");
-}
-const colorChanger = (color) => {
-    container.style.backgroundColor = color;
+    const color = colorInput.value;
+    colorChanger(color);
 }
 
 randombtn.addEventListener('click', rondombtnclick);
 applybtn.addEventListener('click', applybtnclick);
-
-colorChanger('black');
